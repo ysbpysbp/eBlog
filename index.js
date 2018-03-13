95ddd7145fdd451ea9ccfb27ddbb0db66b9079dd
sunzhiyong036 <785037725@qq.com>
2018-03-13 10:14:20 +0800

    初步走通流程

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const Koa = require("koa");
-const mared = require("marked");
+const marked = require("marked");
+
+const fs = require("fs");
 
 const app = new Koa();
+
+const readFile = function (filename) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename,'utf-8',  (err, data) => {
+      if(err) throw err;
+      const datas = marked(data.toString());
+      resolve(datas);
+    });
+  });
+};
+
+app.use(async ctx => {
+  const fileData = await readFile('./blogAssets/test.md');
+  ctx.response.body = fileData;
+});
+
+app.listen(3000);
 
 
